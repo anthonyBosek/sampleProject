06cad526002e03ee44ff8e906d577429557850da
Anthony Bosek <anthony.bosek@gmail.com>
2023-04-20 12:23:15 -0700

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <h1>Sample Project</h1>
     </div>
   );
